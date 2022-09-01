9748c4289a3f93d16531052158729b378868925b
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-01 13:59:07 -0300

    feat: create arrayReplace.js

---
+++ src/arrayReplace.js
@@ -0,0 +1,15 @@
+function solution(inputArray, elemToReplace, substitutionElem) {
+    return inputArray.map((item) => {
+        if (item === elemToReplace) {
+            return substitutionElem;
+        }
+        return item;
+    });
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test arrayReplace
